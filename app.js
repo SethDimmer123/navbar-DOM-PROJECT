4c31f280209844948d15010f3770553355757175
Seth DImmer <sethdimmer1@gmail.com>
2023-02-20 11:48:17 -0500

    initial commit

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// TOGGLE FUNCTIONAILITY USING JAVASCRIPT
+
 // classList - shows/gets all classes an element has.
 // contains - checks classList for specific class
 // add - add class
@@ -20,6 +22,53 @@ navToggle.addEventListener("click", function() {
     console.log(links.classList.contains('links'));
     // in this case on line 20 i should get true because links classes EXISTS 
     // on my unordered list.
+
+
+    // LINES 30-38 IS HOW I TOGGLE MY LINKS BY CLICKING THE HAMBURGER BUTTON.
+
+    // LINE 40 SHOW ME AN EASIER WAY TO TOGGLE THE HAMBURGER BUTTON 
+
+
+    // if(links.classList.contains('show-links')){ // if the links class contains another class alongside links called show-links then it is true
+    //     links.classList.remove('show-links')
+    // }// if show links is another class and true in the unordered list alongside .links then remove show links
+
+    // // OTHERWISE(else) add show-links class to the unordered list alongside ul class="links"
+    // // ul class="link show-links"
+    // else{
+    //     links.classList.add("show-links")
+    // }
+
+    links.classList.toggle("show-links")
+    // I AM ACCESSING THE unordered list called links
+    // using classList to check and to show me all of the classes that are in the unordered list called
+    // links
 });
+
+
+
+// IMPORTANT PART WHEN USING THIS FOR NAVBAR(6:27)
+
+// WHEN I AM ON A BIGGER SCREEN AND SETTING UP THE MEDIA QUERIES
+
+// FOR THE .links i have to add height:auto (look at styles.css LINE 228)
+
+// IF I DONT ADD THE height:auto;
+
+// i have a weird height for my links
+// because i added the show-links class
+
+// the height for the show-links class was 10rem or 160px.
+// that is why the links are massive and 
+// if i move to a bigger screen the link will not show up because by 
+// DEFAULT the link are not OPEN the overflow hidden in styles.css will take over
+// and the height of 0
+
+
+
+
+
+
+
 
 
