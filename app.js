bbef86d8daf135d98296c41ebfff7becad2fe02c
Seth DImmer <sethdimmer1@gmail.com>
2023-02-20 10:03:12 -0500

    initial commit

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
-// classList - shows/gets all classes
+// classList - shows/gets all classes an element has.
 // contains - checks classList for specific class
 // add - add class
 // remove - remove class
 // toggle - toggles class
+
+
+const navToggle = document.querySelector('.nav-toggle') // targeting hamburger menu button
+const links = document.querySelector('.links') //targeting unordered list
+
+navToggle.addEventListener("click", function() {
+    console.log(links.classList);
+});
+
+
